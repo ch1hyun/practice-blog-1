d90a14de49be4ee8c2affb5d8d1f81b6047bae66
chyun <och2510@gmail.com>
2024-07-19 22:46:19 +0900

    feat: 게시물 리스트 아이템 생성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import BoardListItem from 'components/BoardItem';
+import latestBoardListMock from 'mocks/latest-board-list.mock';
 
 function App() {
   return (
-    <></>
+    <>
+    {latestBoardListMock.map(boardListItem => <BoardListItem boardListItem={boardListItem}/>)}
+    </>
   );
 }
 
