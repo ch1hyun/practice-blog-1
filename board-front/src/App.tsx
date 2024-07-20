706db8aceda4484654e5f1c8813216346c165c42
chyun <och2510@gmail.com>
2024-07-20 20:40:49 +0900

    feat: FE 좋아요, 댓글, 글 리스트, 상위 3 리스트 추가

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 import BoardListItem from 'components/BoardItem';
 import latestBoardListMock from 'mocks/latest-board-list.mock';
+import Top3Item from 'components/Top3Item';
+import top3BoardListMock from 'mocks/top-3-board-list.mock';
+import CommentItem from 'components/CommentItem';
+import commentListMock from 'mocks/comment-list.mock';
+import favoriteListMock from 'mocks/favorite-list.mock';
+import FavoriteItem from 'components/FavoriteItem';
 
 function App() {
   return (
     <>
-    {latestBoardListMock.map(boardListItem => <BoardListItem boardListItem={boardListItem}/>)}
+    <div style={{ display: 'flex', columnGap: '30px', rowGap: '20px' }}>
+      {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
+    </div>
     </>
   );
 }
---
+++ board-front/src/mocks/top-3-board-list.mock.ts
@@ -0,0 +1,42 @@
+import { BoardListItem } from "types/interface";
+
+const top3BoardListMock: BoardListItem[] = [
+    {
+        boardNumber: 1,
+        title: "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+        content: "오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까?",
+        boardTitleImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_0w-v0yyABONogMQfQwXLVrOwreSkwMPCiA&s",
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2023.08.18. 00:54:27",
+        writerNickname: "안녕하세요나는주코야키",
+        writerProfileImage: "https://blog.kakaocdn.net/dn/br7xd6/btrbgoJtWYz/GHJ8O3qthPAMmRcUi4Uf6K/img.gif",
+    },
+    {
+        boardNumber: 1,
+        title: "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+        content: "오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까?",
+        boardTitleImage: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2023.08.18. 00:54:27",
+        writerNickname: "안녕하세요나는주코야키",
+        writerProfileImage: null,
+    },
+    {
+        boardNumber: 1,
+        title: "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+        content: "오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까?",
+        boardTitleImage: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2023.08.18. 00:54:27",
+        writerNickname: "안녕하세요나는주코야키",
+        writerProfileImage: null,
+    },
+];
+
+export default top3BoardListMock;
