6a7af6b42e4ca30b18e342eed12836e4885f6964
chyun <och2510@gmail.com>
2024-07-22 18:04:06 +0900

    feat: FE 라우팅 기능 추가

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,23 +1,49 @@
-import React, { useState } from 'react';
+import Footer from 'layouts/\bFooter';
 import './App.css';
-import BoardListItem from 'components/BoardItem';
-import latestBoardListMock from 'mocks/latest-board-list.mock';
-import Top3Item from 'components/Top3Item';
-import top3BoardListMock from 'mocks/top-3-board-list.mock';
-import CommentItem from 'components/CommentItem';
-import commentListMock from 'mocks/comment-list.mock';
-import favoriteListMock from 'mocks/favorite-list.mock';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+import { Route, Routes } from 'react-router-dom';
+import Main from 'views/Main';
+import Authentication from 'views/Authentication';
+import Search from 'views/Search';
+import User from 'views/User';
+import BoardDetail from 'views/Board/Detail';
+import BoardWrite from 'views/Board/Write';
+import BoardUpdate from 'views/Board/Update';
+import Container from 'layouts/Container';
+import { MAIN_PATH } from 'constant';
+import { AUTH_PATH } from 'constant';
+import { SEARCH_PATH } from 'constant';
+import { USER_PATH } from 'constant';
+import { BOARD_PATH } from 'constant';
+import { BOARD_WRITE_PATH } from 'constant';
+import { BOARD_DETAIL_PATH } from 'constant';
+import { BOARD_UPDATE_PATH } from 'constant';
 
+// component: Application 컴포넌트
 function App() {
 
-  const [value, setValue] = useState<string>('');
-
+  // render: Application 컴포넌트 렌더링
+  // description: 메인화면 : '/' - Main
+  // description: 로그인 + 회원가입 : '/auth' - Authentication
+  // description: 검색화면 : '/search/:searchWord' - Search
+  // description: 유저 페이지 : '/user/:userEmail' - User
+  // description: 게시물 상세보기 : '/board/detail/:boardNumber' - BoardDetail
+  // description: 게시물 작성하기 : '/board/write' - BoardWrite
+  // description: 게시물 수정하기 : '/board/update/:boardNumber' - BoardUpdate
   return (
-    <>
-    <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력하세요.' value={value} error={true} setValue={setValue} message='올바른 이메일 형식으로 작성해주세요.'/>
-    </>
+    <Routes>
+      <Route element={<Container />}>
+        <Route path={MAIN_PATH()} element={<Main />}/>
+        <Route path={AUTH_PATH()} element={<Authentication />}/>
+        <Route path={SEARCH_PATH(':searchWord')} element={<Search />}/>
+        <Route path={USER_PATH(':userEmail')} element={<User />}/>
+        <Route path={BOARD_PATH()}>
+          <Route path={BOARD_WRITE_PATH()} element={<BoardDetail />}/>
+          <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardWrite />}/>
+          <Route path={BOARD_UPDATE_PATH(':boardNumber')} element={<BoardUpdate />}/>
+        </Route>
+        <Route path='*' element={<h1>404 Not Found</h1>}/>
+      </Route>
+    </Routes>
   );
 }
 
